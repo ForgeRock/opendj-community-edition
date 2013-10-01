e16b814975dcf38ef98cb8e65b7ea814f786fa22
Chris Ridd <chris.ridd@forgerock.com>
2013-10-01 15:00:17 +0000

    Update copyright

--- src/server/org/opends/server/core/GroupManager.java
+++ src/server/org/opends/server/core/GroupManager.java
@@ -23,7 +23,7 @@
  *
  *
  *      Copyright 2007-2010 Sun Microsystems, Inc.
- *      Portions Copyright 2011 ForgeRock AS
+ *      Portions Copyright 2011-2013 ForgeRock AS
  */
 package org.opends.server.core;
 
